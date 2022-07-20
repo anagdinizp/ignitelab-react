f163147b8e8e6a5b02d2ec9ecfdbacb29e3e2578
anagdinizp <anagabriela0399@hotmail.com>
2022-07-19 22:17:02 -0300

    HTML e CSS da página de inscrição

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4u50z0x0lno01t73gfcd2rk/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_APO_ACESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
